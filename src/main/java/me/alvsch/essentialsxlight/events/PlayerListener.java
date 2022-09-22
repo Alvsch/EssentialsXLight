3a5aa0648130bfee49ae53bce7e090195b5ee9d5
Alvsch <alvin.scherdin@gmail.com>
2022-09-22 17:25:15 +0200

    Added custom join and quit messages and custom motd

--- src/main/java/me/alvsch/essentialsxlight/events/PlayerListener.java
+++ src/main/java/me/alvsch/essentialsxlight/events/PlayerListener.java
@@ -6,6 +6,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Objects;
 
 public class PlayerListener implements Listener {
 
@@ -29,6 +32,43 @@ public class PlayerListener implements Listener {
             player.sendMessage(Utils.color("&4This version of " + plugin.getName() + " is unstable"));
         }
 
+
+        String message;
+        if(player.hasPlayedBefore()) {
+            message = plugin.config.getString("custom-join-message");
+        } else {
+            message = plugin.config.getString("custom-first-join-message");
+        }
+
+        if(message == null) {
+            e.setJoinMessage("");
+        } else {
+            if (!Objects.equals(message, "none")) {
+                e.setJoinMessage(message
+                        .replace("{USERNAME}", player.getName())
+                        .replace("{PLAYER}", player.getDisplayName())
+                );
+            }
+        }
+
+    }
+
+    // PlayerQuitEvent
+    @EventHandler
+    public void playerQuit(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+
+        String message = plugin.config.getString("custom-quit-message");
+        if(message == null) {
+            e.setQuitMessage("");
+        } else {
+            if (!Objects.equals(message, "none")) {
+                e.setQuitMessage(message
+                        .replace("{USERNAME}", player.getName())
+                        .replace("{PLAYER}", player.getDisplayName())
+                );
+            }
+        }
     }
 
 
--- src/main/java/me/alvsch/essentialsxlight/events/ServerListener.java
+++ src/main/java/me/alvsch/essentialsxlight/events/ServerListener.java
@@ -1,14 +1,29 @@
 package me.alvsch.essentialsxlight.events;
 
 import me.alvsch.essentialsxlight.EssentialsXLight;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
 
 public class ServerListener implements Listener {
+
     EssentialsXLight plugin;
 
     public ServerListener(EssentialsXLight plugin) {
         this.plugin = plugin;
     }
 
+    @EventHandler
+    public void serverListPing(ServerListPingEvent e) {
+        String motd = plugin.config.getString("custom-motd");
+        if(motd == null) {
+            e.setMotd("");
+        } else {
+            if(!motd.equals("none")) {
+                e.setMotd(motd);
+            }
+
+        }
+    }
 
 }
